fd41fbdd5aee54d4ee2bddac26ad99239d8a7d5d
scatter-dev <christianlegge888@gmail.com>
2020-08-04 01:54:21 -0400

    added audio registers

--- HappyBoy/APU.h
+++ HappyBoy/APU.h
@@ -1,5 +1,162 @@
 #pragma once
+
+#include <cstdint>
+
 class APU
 {
+public:
+	union {
+		struct {
+			uint8_t sweep_shift : 3;
+			bool increase_decrease : 1;
+			uint8_t sweep_time : 3;
+			bool unused : 1;
+		};
+		uint8_t reg;
+	} NR10;
+	union {
+		struct {
+			uint8_t sound_length : 6;
+			uint8_t duty : 2;
+		};
+		uint8_t reg;
+	} NR11;
+	union {
+		struct {
+			uint8_t sweep : 3;
+			bool direction : 1;
+			uint8_t volume : 4;
+		};
+		uint8_t reg;
+	} NR12;
+	uint8_t NR13;
+	union {
+		struct {
+			uint8_t frequency : 3;
+			uint8_t unused : 3;
+			bool counter : 1;
+			bool initial : 1;
+		};
+		uint8_t reg;
+	} NR14;
+
+	union {
+		struct {
+			uint8_t length : 6;
+			uint8_t duty : 2;
+		};
+		uint8_t reg;
+	} NR21;
+	union {
+		struct {
+			uint8_t sweep : 3;
+			bool direction : 1;
+			uint8_t volume : 4;
+		};
+		uint8_t reg;
+	} NR22;
+	uint8_t NR23;
+	union {
+		struct {
+			uint8_t frequency : 3;
+			uint8_t unused : 3;
+			bool counter : 1;
+			bool initial : 1;
+		};
+		uint8_t reg;
+	} NR24;
+
+	union {
+		struct {
+			uint8_t unused : 7;
+			bool enable : 1;
+		};
+		uint8_t reg;
+	} NR30;
+	uint8_t NR31;
+	union {
+		struct {
+			uint8_t unused : 5;
+			uint8_t volume : 2;
+			bool unused2 : 1;
+		};
+		uint8_t reg;
+	} NR32;
+	uint8_t NR33;
+	union {
+		struct {
+			uint8_t frequency : 3;
+			uint8_t unused : 3;
+			bool counter : 1;
+			bool initial : 1;
+		};
+		uint8_t reg;
+	} NR34;
+
+	union {
+		struct {
+			uint8_t length : 6;
+			uint8_t unused : 2;
+		};
+		uint8_t reg;
+	} NR41;
+	union {
+		struct {
+			uint8_t sweep : 3;
+			bool direction : 1;
+			uint8_t volume : 4;
+		};
+		uint8_t reg;
+	} NR42;
+	union {
+		struct {
+			uint8_t ratio : 3;
+			bool width : 1;
+			uint8_t frequency : 4;
+		};
+		uint8_t reg;
+	} NR43;
+	union {
+		struct {
+			uint8_t unused : 6;
+			bool counter : 1;
+			bool initial : 1;
+		};
+		uint8_t reg;
+	} NR44;
+
+	union {
+		struct {
+			uint8_t so1_volume : 3;
+			bool so1_vin : 1;
+			uint8_t so2_volume : 3;
+			bool so2_vin : 1;
+		};
+		uint8_t reg;
+	} NR50;
+	union {
+		struct {
+			bool sound1_to_so1 : 1;
+			bool sound2_to_so1 : 1;
+			bool sound3_to_so1 : 1;
+			bool sound4_to_so1 : 1;
+			bool sound1_to_so2 : 1;
+			bool sound2_to_so2 : 1;
+			bool sound3_to_so2 : 1;
+			bool sound4_to_so2 : 1;
+		};
+		uint8_t reg;
+	} NR51;
+	union {
+		struct {
+			bool sound1_on : 1;
+			bool sound2_on : 1;
+			bool sound3_on : 1;
+			bool sound4_on : 1;
+			uint8_t unused : 3;
+			bool allsound_on : 1;
+		};
+		uint8_t reg;
+	} NR52;
 };
 
--- HappyBoy/Bus.cpp
+++ HappyBoy/Bus.cpp
@@ -56,6 +56,69 @@ uint8_t Bus::read(uint16_t addr)
 	else if (addr == 0xFF0F) {
 		return cpu->IF.reg;
 	}
+	else if (addr == 0xFF10) {
+		return apu->NR10.reg;
+	}
+	else if (addr == 0xFF11) {
+		return apu->NR11.reg;
+	}
+	else if (addr == 0xFF12) {
+		return apu->NR12.reg;
+	}
+	else if (addr == 0xFF13) {
+		return 0xFF;
+	}
+	else if (addr == 0xFF14) {
+		return apu->NR14.reg;
+	}
+	else if (addr == 0xFF16) {
+		return apu->NR21.reg;
+	}
+	else if (addr == 0xFF17) {
+		return apu->NR22.reg;
+	}
+	else if (addr == 0xFF18) {
+		return 0xFF;
+	}
+	else if (addr == 0xFF19) {
+		return apu->NR24.reg;
+	}
+	else if (addr == 0xFF1A) {
+		return apu->NR30.reg;
+	}
+	else if (addr == 0xFF1B) {
+		return apu->NR31;
+	}
+	else if (addr == 0xFF1C) {
+		return apu->NR32.reg;
+	}
+	else if (addr == 0xFF1D) {
+		return 0xFF;
+	}
+	else if (addr == 0xFF1E) {
+		return apu->NR34.reg;
+	}
+	else if (addr == 0xFF20) {
+		return apu->NR41.reg;
+	}
+	else if (addr == 0xFF21) {
+		return apu->NR42.reg;
+	}
+	else if (addr == 0xFF22) {
+		return apu->NR43.reg;
+	}
+	else if (addr == 0xFF23) {
+		return apu->NR44.reg;
+	}
+	else if (addr == 0xFF24) {
+		return apu->NR50.reg;
+	}
+	else if (addr == 0xFF25) {
+		return apu->NR51.reg;
+	}
+	else if (addr == 0xFF26) {
+		return apu->NR52.reg;
+	}	
 	else if (addr == 0xFF40) {
 		return ppu->LCDC.reg;
 	}
@@ -113,6 +176,69 @@ void Bus::write(uint16_t addr, uint8_t data)
 	}
 	else if (addr == 0xFF0F) {
 		cpu->IF.reg = data;
+	}
+	else if (addr == 0xFF10) {
+		apu->NR10.reg = data;
+	}
+	else if (addr == 0xFF11) {
+		apu->NR11.reg = data;
+	}
+	else if (addr == 0xFF12) {
+		apu->NR12.reg = data;
+	}
+	else if (addr == 0xFF13) {
+		apu->NR13 = data;
+	}
+	else if (addr == 0xFF14) {
+		apu->NR14.reg = data;
+	}
+	else if (addr == 0xFF16) {
+		apu->NR21.reg = data;
+	}
+	else if (addr == 0xFF17) {
+		apu->NR22.reg = data;
+	}
+	else if (addr == 0xFF18) {
+		apu->NR23 = data;
+	}
+	else if (addr == 0xFF19) {
+		apu->NR24.reg = data;
+	}
+	else if (addr == 0xFF1A) {
+		apu->NR30.reg = data;
+	}
+	else if (addr == 0xFF1B) {
+		apu->NR31 = data;
+	}
+	else if (addr == 0xFF1C) {
+		apu->NR32.reg = data;
+	}
+	else if (addr == 0xFF1D) {
+		apu->NR33 = data;
+	}
+	else if (addr == 0xFF1E) {
+		apu->NR34.reg = data;
+	}
+	else if (addr == 0xFF20) {
+		apu->NR41.reg = data;
+	}
+	else if (addr == 0xFF21) {
+		apu->NR42.reg = data;
+	}
+	else if (addr == 0xFF22) {
+		apu->NR43.reg = data;
+	}
+	else if (addr == 0xFF23) {
+		apu->NR44.reg = data;
+	}
+	else if (addr == 0xFF24) {
+		apu->NR50.reg = data;
+	}
+	else if (addr == 0xFF25) {
+		apu->NR51.reg = data;
+	}
+	else if (addr == 0xFF26) {
+		apu->NR52.reg |= data & 0x80;
 	}
 	else if (addr == 0xFF40) {
 		ppu->LCDC.reg = data;
