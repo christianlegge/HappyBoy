06cc7dfe4e8ab4c4c0ef0247a3bf3d5465cb3c2d
scatter-dev <christianlegge888@gmail.com>
2020-08-07 00:53:45 -0400

    always on sine wave

--- HappyBoy/Speaker.cpp
+++ HappyBoy/Speaker.cpp
@@ -18,9 +18,18 @@ void Speaker::Start() {
 	audio_spec.freq = 44100;
 	audio_spec.format = AUDIO_S16SYS;
 	audio_spec.channels = 1;
-	audio_spec.samples = 1024;
+	audio_spec.samples = 512;
 	audio_spec.callback = NULL;
+	double time_step = 1.0 / audio_spec.freq;
+	double global_time = 0;
 
 	audio_device = SDL_OpenAudioDevice(NULL, 0, &audio_spec, NULL, 0);
-	SDL_PauseAudioDevice(audio_device, 0);
+	while (true) {
+		for (int i = 0; i < audio_spec.samples; i++) {
+			int16_t sample = (sin(global_time * 440 * 2 * M_PI)) * 3000;
+			SDL_QueueAudio(audio_device, &sample, sizeof(int16_t));
+			global_time += time_step;
+		}
+		SDL_PauseAudioDevice(audio_device, 0);
+	}
 }
