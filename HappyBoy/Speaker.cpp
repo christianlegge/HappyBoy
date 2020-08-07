4c316d9ce0e370d8c18f75ebf5410063c81bde20
scatter-dev <christianlegge888@gmail.com>
2020-08-07 04:16:37 -0400

    using audio callback

--- HappyBoy/Speaker.cpp
+++ HappyBoy/Speaker.cpp
@@ -2,13 +2,45 @@
 
 #include <thread>
 #include <iostream>
-#include <SDL.h>
+
+void audio_callback(void* userdata, Uint8* stream, int len)
+{
+	Speaker* s = (Speaker*)userdata;
+	if (!s->apu->NR52.allsound_on) {
+		return;
+	}
+	double channel1_freq = 0;
+	if (!channel1.initial && s->apu->NR14.initial) {
+		channel1.playing = true;
+		channel1.start_time = s->global_time;
+		channel1.length = s->apu->NR11.sound_length;
+	}
+	channel1.l_volume = s->apu->NR51.sound1_to_so1 ? s->apu->NR50.so1_volume : 0;
+	channel1.r_volume = s->apu->NR51.sound1_to_so2 ? s->apu->NR50.so2_volume : 0;
+	channel1.initial = s->apu->NR14.initial;
+	channel1.constant = !s->apu->NR14.counter;
+	channel1.freq = 131072.0 / (2048 - (((s->apu->NR14.reg & 0x07) << 8) + s->apu->NR13));
+
+	channel2.l_volume = s->apu->NR51.sound2_to_so1 ? s->apu->NR50.so1_volume : 0;
+	channel2.r_volume = s->apu->NR51.sound2_to_so2 ? s->apu->NR50.so2_volume : 0;
+	channel2.initial = s->apu->NR24.initial;
+	channel2.constant = !s->apu->NR24.counter;
+	channel2.freq = 131072.0 / (2048 - (((s->apu->NR24.reg & 0x07) << 8) + s->apu->NR23));
+
+	for (int i = 0; i < len / 2; i++) {
+		((Uint16*)stream)[i] = channel1.get_sample(s->global_time) + channel2.get_sample(s->global_time);
+		s->global_time += s->time_step;
+	}
+}
+
+
 
 Speaker::Speaker(std::shared_ptr<APU> apu) : apu(apu)
 {
 	std::thread speakerThread(&Speaker::Start, this);
 	speakerThread.detach();
 }
+
 
 void Speaker::Start() {
 	SDL_Init(SDL_INIT_AUDIO);
@@ -18,18 +50,15 @@ void Speaker::Start() {
 	audio_spec.freq = 44100;
 	audio_spec.format = AUDIO_S16SYS;
 	audio_spec.channels = 1;
-	audio_spec.samples = 512;
-	audio_spec.callback = NULL;
-	double time_step = 1.0 / audio_spec.freq;
-	double global_time = 0;
+	audio_spec.samples = 1024;
+	audio_spec.callback = audio_callback;
+	audio_spec.userdata = this;
+	time_step = 1.0 / audio_spec.freq;
+	global_time = 0;
 
 	audio_device = SDL_OpenAudioDevice(NULL, 0, &audio_spec, NULL, 0);
-	while (true) {
-		for (int i = 0; i < audio_spec.samples; i++) {
-			int16_t sample = (sin(global_time * 440 * 2 * M_PI)) * 3000;
-			SDL_QueueAudio(audio_device, &sample, sizeof(int16_t));
-			global_time += time_step;
-		}
-		SDL_PauseAudioDevice(audio_device, 0);
-	}
+
+	SDL_PauseAudioDevice(audio_device, 0);
 }
+
+
--- HappyBoy/Speaker.h
+++ HappyBoy/Speaker.h
@@ -3,14 +3,63 @@
 #include <memory>
 
 #include "APU.h";
+#include <SDL.h>
+
+struct {
+	double length = 0;
+	double start_time;
+	double freq;
+	bool constant = false;
+	bool initial = false;
+	double l_volume = 0;
+	double r_volume = 0;
+	bool playing = false;
+
+	int16_t get_sample(double time) {
+		if (!playing) {
+			return 0;
+		}
+		if (time < start_time + length || constant) {
+			return (sin(time * freq * 2 * M_PI) > 0 ? 1 : -1) * ((l_volume + r_volume) / 14.0) * 1000;
+		}
+		else {
+			return 0;
+		}
+	}
+} channel1;
+
+struct {
+	double length = 0;
+	double start_time;
+	double freq;
+	bool constant = false;
+	bool initial = false;
+	double l_volume = 0;
+	double r_volume = 0;
+	bool playing = false;
+
+	int16_t get_sample(double time) {
+		if (!playing) {
+			return 0;
+		}
+		if (time < start_time + length || constant) {
+			return (sin(time * freq * 2 * M_PI) > 0 ? 1 : -1) * ((l_volume + r_volume) / 14.0) * 1000;
+		}
+		else {
+			return 0;
+		}
+	}
+} channel2;
 
 class Speaker
 {
 public:
 	Speaker(std::shared_ptr<APU> apu);
+	std::shared_ptr<APU> apu;
+	double global_time;
+	double time_step;
 
 private:
 	void Start();
-	std::shared_ptr<APU> apu;
 };
 
